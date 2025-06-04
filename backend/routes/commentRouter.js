0dfaa50357f3854214fbe58fa73663ae2020060b
DungPTIT <141023874+doducdung2004@users.noreply.github.com>
2025-06-04 22:33:51 +0700

    Update commentRouter.js

--- backend/routes/commentRouter.js
+++ backend/routes/commentRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Photo = require("../db/PhotoModel");
+const Photo = require("../db/photoModel");
 
 function authMiddleware(req, res, next) {
     if (req.session && req.session.user) {
